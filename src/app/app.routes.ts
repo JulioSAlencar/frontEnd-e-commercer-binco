0981587863b0d142cc59627012fa85f2c0e82fce
IcaroMoreir4 <icaromoreira90@gmail.com>
2025-01-10 18:40:10 -0300

    fix
    git commit -m fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from './pages/home/home.component'; // Certifique-se de
 import { UserProfileComponent } from './pages/user-profile/user-profile.component';
 import { Error404Component } from './error/error-404/error-404.component';
 import { PreferencesComponent } from './pages/preferences/preferences.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 export const routes: Routes = [
   // { path: 'users', component: UserProfileComponent},
@@ -13,6 +14,7 @@ export const routes: Routes = [
       { path: 'preferences', component: PreferencesComponent },
     ],
   },
+  { path: 'register', component: RegisterComponent },
   { path: '404', component: Error404Component },
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: '**', redirectTo: '404' }, // Redireciona para a página inicial para rotas inválidas
